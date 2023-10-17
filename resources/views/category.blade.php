1166b715d23b1e2c4773323dcf7752ac94f97ec8
akamzrk <mizerk87@gmail.com>
2023-10-17 20:09:22 +0900

    カテゴリ、トラブル、トラブル詳細のSP版実装

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -101,9 +101,124 @@
 
 @section('content_sp')
 
+<div class="category_top">
+    <h2>CATEGORY</h2>
+    <select>
+        <option>新着</option>
+        <option>おすすめ</option>
+        <option>価格の高い商品</option>
+        <option>価格の低い商品</option>
+        <option>お気に入り数</option>
+    </select>
+</div>
 
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
 
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
 
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
+
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
+
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
+
+<div class="category_flex">
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item1.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item2.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+        <a href="{{ route('item_detail') }}" class="category_item_a">
+            <img src="{{ asset('img/item3.png') }}" class="top3_item_img">
+            <div class="top3_item_name">商品名</div>
+            <div class="top3_item_name">￥1,000</div>
+        </a>
+</div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/work.js') }}"></script>
--- resources/views/trouble.blade.php
+++ resources/views/trouble.blade.php
@@ -36,5 +36,24 @@
 
 @section('content_sp')
 
+<div class="trouble_div">
+    <div class="trouble_title">
+        <h2>TROUBLE</h2>
+        <h3>お悩みから探す</h3>
+    </div>
+    <div class="trouble_wrapper">
+        <img src="{{ asset('img/trouble_main_img.png') }}" class="trouble_main_img">
+        <div class="trouble_list">
+            <a href="{{ route('trouble_detail') }}" class="trouble_a">気になる黒ずみや嫌なニオイ</a>
+            <a href="" class="trouble_a">おりもの量の増加</a>
+            <a href="" class="trouble_a">美肌効果を得たい</a>
+            <a href="" class="trouble_a">かぶれやかゆみ<br>乾燥によるお肌のトラブル</a>
+            <a href="" class="trouble_a">年齢や疲れによるたるみ</a>
+        </div>        
+    </div>
+
+    @include('item.consultation')
+
+</div>
 
 @endsection
