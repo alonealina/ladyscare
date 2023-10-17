b2a29c83f4859d64332056595df9d24b147f2041
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-10-17 15:58:48 +0900

    SP版　パスワード再設定系　実装

--- resources/views/pass_rev_comp.blade.php
+++ resources/views/pass_rev_comp.blade.php
@@ -5,8 +5,6 @@
     <div class="category_title">パスワード再設定</span></div>
 
     <div class="comp_text">パスワードを再設定しました。<br>元の画面に戻ります。</div>
-
-
 </div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/pass_rev.js') }}"></script>
@@ -16,28 +14,14 @@
 
 
 @section('content_sp')
-<div class="contact_div_sp">
-    <form name="mail_form" action="{{ route('mail_send') }}" method="post" enctype="multipart/form-data">
-    @csrf
-        <div class="page_title_sp">CONTACT</div>
+<div class="page_title_sp">パスワード再設定</div>
 
-        <div class="contact_name">Name</div>
-        <input class="form_text_sp" placeholder="" name="name" type="text" id="name">
+<div class="mail_comp" style="text-align:center;">パスワードを再設定しました。<br>元の画面に戻ります。</div>
 
-        <div class="contact_name">Phone number</div>
-        <input class="form_text_sp" placeholder="" name="tel" type="text" id="tel">
+<a href="{{ route('index') }}" class="send_btn">TOPに戻る</a>
 
-        <div class="contact_name">E-mail</div>
-        <input class="form_text_sp" placeholder="" name="mail" type="text" id="mail">
+<div class="contact_bottom_padding"></div>
 
-        <div class="contact_name">Messsge</div>
-        <textarea class="form_textarea_sp" name="content" id="content"></textarea>
-        <div class="error_message_sp" id="error_message"></div>
-        <div class="send_button" style="margin-top:20px;">
-            <a href="#" onclick="clickMailButton()" style="margin:auto;">SEND</a>
-        </div>
-    </form>
-</div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/contact.js') }}"></script>
 
