39b0f2ddf05be36a3e9a1432678b5929c4cdf544
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-10-17 16:01:42 +0900

    SP版　ログイン画面実装

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -29,29 +29,27 @@
 
 
 @section('content_sp')
-<div class="contact_div_sp">
-    <form name="mail_form" action="{{ route('mail_send') }}" method="post" enctype="multipart/form-data">
+<div class="page_title_sp">ログイン</div>
+
+<div class="reminder_text">ご登録のメールアドレスを入力して送信してください。</div>
+<form name="login_form" action="{{ route('reminder_send') }}" method="post" enctype="multipart/form-data">
     @csrf
-        <div class="page_title_sp">CONTACT</div>
+    <div class="login_div">
+        <div class="contact_name">メールアドレス</div>
+        <input class="contact_text" placeholder="" name="mail" type="text" id="mail">
+    </div>
+    <div class="login_div">
+        <div class="contact_name">パスワード</div>
+        <input class="contact_text" placeholder="" name="pass" type="password" id="pass">
+    </div>
+    <a href="#!" onclick="clickLoginButton()" class="send_btn">ログイン</a>
 
-        <div class="contact_name">Name</div>
-        <input class="form_text_sp" placeholder="" name="name" type="text" id="name">
+    <a href="{{ route('reminder') }}" class="reminder_link">パスワードをお忘れですか？</a>
+    <div class="error_message" id="error_message"></div>
+</form>
+<div class="contact_bottom_padding"></div>
 
-        <div class="contact_name">Phone number</div>
-        <input class="form_text_sp" placeholder="" name="tel" type="text" id="tel">
-
-        <div class="contact_name">E-mail</div>
-        <input class="form_text_sp" placeholder="" name="mail" type="text" id="mail">
-
-        <div class="contact_name">Messsge</div>
-        <textarea class="form_textarea_sp" name="content" id="content"></textarea>
-        <div class="error_message_sp" id="error_message"></div>
-        <div class="send_button" style="margin-top:20px;">
-            <a href="#" onclick="clickMailButton()" style="margin:auto;">SEND</a>
-        </div>
-    </form>
-</div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="{{ asset('js/contact.js') }}"></script>
+<script src="{{ asset('js/login.js') }}"></script>
 
 @endsection
