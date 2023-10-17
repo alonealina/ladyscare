e71786f1e91166c597dacc2b758b6565d48c1dc2
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-10-17 15:31:06 +0900

    SP版　問い合わせ系実装

--- resources/views/mail_comp.blade.php
+++ resources/views/mail_comp.blade.php
@@ -13,8 +13,9 @@
 
 
 @section('content_sp')
+<div class="page_title_sp">お問い合わせ</div>
 
-<div class="mail_comp">お問い合わせありがとうございます。<br>返信が届くまでしばらくお待ちください。</div>
+<div class="mail_comp">お問い合わせありがとうございます。<br>返信が届くまで3営業程度かかる場合がございますので<br>しばらくお待ちください。</div>
 
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/contact.js') }}"></script>
