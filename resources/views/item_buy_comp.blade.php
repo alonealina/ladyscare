66cdecd1ace7f744c97a39c6efca3a745bea24e5
bluesky2008 <william_billl2008@yahoo.co.jp>
2023-10-17 16:47:54 +0900

    SP版　購入画面関連実装

--- resources/views/item_buy_comp.blade.php
+++ resources/views/item_buy_comp.blade.php
@@ -23,28 +23,17 @@
 
 
 @section('content_sp')
-<div class="contact_div_sp">
-    <form name="mail_form" action="{{ route('mail_send') }}" method="post" enctype="multipart/form-data">
-    @csrf
-        <div class="page_title_sp">CONTACT</div>
+<div class="page_title_sp">ご注文内容確認</div>
 
-        <div class="contact_name">Name</div>
-        <input class="form_text_sp" placeholder="" name="name" type="text" id="name">
-
-        <div class="contact_name">Phone number</div>
-        <input class="form_text_sp" placeholder="" name="tel" type="text" id="tel">
-
-        <div class="contact_name">E-mail</div>
-        <input class="form_text_sp" placeholder="" name="mail" type="text" id="mail">
-
-        <div class="contact_name">Messsge</div>
-        <textarea class="form_textarea_sp" name="content" id="content"></textarea>
-        <div class="error_message_sp" id="error_message"></div>
-        <div class="send_button" style="margin-top:20px;">
-            <a href="#" onclick="clickMailButton()" style="margin:auto;">SEND</a>
-        </div>
-    </form>
+<div class="mail_comp">
+    ご注文ありがとうございます。<br>
+    ご入力いただいたメールアドレスにご注文確定メールが届きましたらお支払いをお願いいたします。
 </div>
+
+<a href="{{ route('index') }}" class="send_btn">TOPに戻る</a>
+
+<div class="contact_bottom_padding"></div>
+
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/contact.js') }}"></script>
 
--- resources/views/paymethod_select.blade.php
+++ resources/views/paymethod_select.blade.php
@@ -4,16 +4,16 @@
 <div class="category_div">
     <div class="category_title">お支払方法選択</span></div>
     <div class="pay_div">
-        <input type="radio" class="radio_pink" name="" value="銀行振込">銀行振込<br>
-        <input type="radio" class="radio_pink" name="" value="クレジットカード">クレジットカード<br>
-        <input type="radio" class="radio_pink" name="" value="代金引換">代金引換<span class="charge_span">(手数料250円税込)</span><br>
-        <input type="radio" class="radio_pink" name="" value="コンビニ後払い">コンビニ後払い<span class="charge_span">(手数料250円税込)</span><br>
-        <input type="radio" class="radio_pink" name="" value="Paidy後払い">Paidy後払い<span class="charge_span">(手数料250円税込)</span><br>
-        <input type="radio" class="radio_pink" name="" value="バーコード決済">バーコード決済<br>
-        <input type="radio" class="radio_pink" name="" value="キャリア決済">キャリア決済<br>
+        <input type="radio" class="radio_pink" name="method" value="銀行振込">銀行振込<br>
+        <input type="radio" class="radio_pink" name="method" value="クレジットカード">クレジットカード<br>
+        <input type="radio" class="radio_pink" name="method" value="代金引換">代金引換<span class="charge_span">(手数料250円税込)</span><br>
+        <input type="radio" class="radio_pink" name="method" value="コンビニ後払い">コンビニ後払い<span class="charge_span">(手数料250円税込)</span><br>
+        <input type="radio" class="radio_pink" name="method" value="Paidy後払い">Paidy後払い<span class="charge_span">(手数料250円税込)</span><br>
+        <input type="radio" class="radio_pink" name="method" value="バーコード決済">バーコード決済<br>
+        <input type="radio" class="radio_pink" name="method" value="キャリア決済">キャリア決済<br>
 
         <div class="buy_btn_flex" style="margin: 0;">
-            <a href="#" class="buy_btn_white" style="padding: 1.25vw;" onclick="history.back()">戻る</span></a>
+            <a href="#" class="buy_btn_white" style="padding: 1.25vw;" onclick="history.back()">戻る</a>
             <a href="{{ route('item_buy_comp') }}" class="buy_btn_black" style="padding: 1.25vw;">ご注文内容確認へ</a>
         </div>
 
@@ -28,28 +28,22 @@
 
 
 @section('content_sp')
-<div class="contact_div_sp">
-    <form name="mail_form" action="{{ route('mail_send') }}" method="post" enctype="multipart/form-data">
-    @csrf
-        <div class="page_title_sp">CONTACT</div>
+<div class="page_title_sp">お支払方法選択</div>
 
-        <div class="contact_name">Name</div>
-        <input class="form_text_sp" placeholder="" name="name" type="text" id="name">
+<div class="pay_div">
+    <input type="radio" class="radio_pink" name="method" value="銀行振込">銀行振込<br>
+    <input type="radio" class="radio_pink" name="method" value="クレジットカード">クレジットカード<br>
+    <input type="radio" class="radio_pink" name="method" value="代金引換">代金引換<span class="charge_span">(手数料250円税込)</span><br>
+    <input type="radio" class="radio_pink" name="method" value="コンビニ後払い">コンビニ後払い<span class="charge_span">(手数料250円税込)</span><br>
+    <input type="radio" class="radio_pink" name="method" value="Paidy後払い">Paidy後払い<span class="charge_span">(手数料250円税込)</span><br>
+    <input type="radio" class="radio_pink" name="method" value="バーコード決済">バーコード決済<br>
+    <input type="radio" class="radio_pink" name="method" value="キャリア決済">キャリア決済<br>
 
-        <div class="contact_name">Phone number</div>
-        <input class="form_text_sp" placeholder="" name="tel" type="text" id="tel">
+    <a href="#!" class="send_btn send_btn_white" style="margin:20px auto;" onclick="history.back()">戻る</a>
+    <a href="{{ route('item_buy_comp') }}" class="send_btn" style="margin:20px auto;">ご注文内容確認へ</a>
 
-        <div class="contact_name">E-mail</div>
-        <input class="form_text_sp" placeholder="" name="mail" type="text" id="mail">
-
-        <div class="contact_name">Messsge</div>
-        <textarea class="form_textarea_sp" name="content" id="content"></textarea>
-        <div class="error_message_sp" id="error_message"></div>
-        <div class="send_button" style="margin-top:20px;">
-            <a href="#" onclick="clickMailButton()" style="margin:auto;">SEND</a>
-        </div>
-    </form>
 </div>
+
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="{{ asset('js/contact.js') }}"></script>
 
